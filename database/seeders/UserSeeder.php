c3b87b7fefed33574876cfed20ed43ca57776f2c
RUF.DEV <aguilarufino@gmail.com>
2025-11-25 13:24:51 +0800

    added user enum

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\UserRoleEnum;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +15,25 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'), // Hash the password
+            'role' => UserRoleEnum::SUPER_ADMIN, // Assign Super Admin role
+        ]);
+        // Inventory Manager User
+        User::create([
+            'name' => 'Manager User',
+            'email' => 'manager@example.com',
+            'password' => Hash::make('password'),
+            'role' => UserRoleEnum::INVENTORY_MANAGER, // Assign Inventory Manager role
+        ]);
+        // Regular Inventory User
+        User::create([
+            'name' => 'Basic User',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+            'role' => UserRoleEnum::INVENTORY_USER, // Assign Inventory User role
+        ]);
     }
 }
