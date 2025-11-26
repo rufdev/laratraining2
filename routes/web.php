147f265473974d7af0e9f127fdd444657db94e33
RUF.DEV <aguilarufino@gmail.com>
2025-11-26 11:58:13 +0800

    added middleware role on web.php/routes

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,22 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('categories', [CategoryController::class, 'index'])->name('categories');
-Route::get('assets', [AssetController::class, 'index'])->name('assets');
-Route::get('manufacturers', [ManufacturerController::class, 'index'])->name('manufacturers');
-Route::get('locations', [LocationController::class, 'index'])->name('locations');
+
+Route::middleware(['auth', 'verified', 'role:super_admin'])->group(function () {
+    Route::get('categories', [CategoryController::class, 'index'])->name('categories');
+});
+
+Route::middleware(['auth', 'verified', 'role:super_admin,inventory_manager'])->group(function () {
+    Route::get('manufacturers', [ManufacturerController::class, 'index'])->name('manufacturers');
+    Route::get('locations', [LocationController::class, 'index'])->name('locations');
+});
+
+Route::middleware(['auth', 'verified', 'role:super_admin,inventory_user'])->group(function () {
+    Route::get('assets', [AssetController::class, 'index'])->name('assets');
+});
+
+
+
+
+
 require __DIR__.'/settings.php';
