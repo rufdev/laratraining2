fbdae2dc7b692358a06b7799dc52e5f83301684a
RUF.DEV <aguilarufino@gmail.com>
2025-11-28 10:24:47 +0800

    added tile/totals

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -5,11 +5,12 @@ import PieChart from '@/components/chartcomponents/PieChart.vue';
 import DoughnutChart from '@/components/chartcomponents/DoughnutChart.vue';
 
 
-import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes';
+import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -219,22 +220,54 @@ onMounted(async () => {
         <div
             class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
         >
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
-                >
-                    <PlaceholderPattern />
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6 xl:grid-cols-4">
+                <!-- Metric Item Start -->
+                <div class="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03]">
+                    <p class="text-theme-sm text-gray-500 dark:text-gray-400">Total Assets</p>
+
+                    <div class="mt-3 flex items-end justify-between">
+                        <div>
+                            <h4 class="text-2xl font-bold text-gray-800 dark:text-white/90">{{ totals.total_assets }}</h4>
+                        </div>
+                    </div>
                 </div>
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
-                >
-                    <PlaceholderPattern />
+                <!-- Metric Item End -->
+
+                <!-- Metric Item Start -->
+                <div class="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03]">
+                    <p class="text-theme-sm text-gray-500 dark:text-gray-400">Total Categories</p>
+
+                    <div class="mt-3 flex items-end justify-between">
+                        <div>
+                            <h4 class="text-2xl font-bold text-gray-800 dark:text-white/90">{{ totals.total_categories }}</h4>
+                        </div>
+                    </div>
                 </div>
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
-                >
-                    <PlaceholderPattern />
+                <!-- Metric Item End -->
+
+                <!-- Metric Item Start -->
+                <div class="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03]">
+                    <p class="text-theme-sm text-gray-500 dark:text-gray-400">Total Manufacturers</p>
+
+                    <div class="mt-3 flex items-end justify-between">
+                        <div>
+                            <h4 class="text-2xl font-bold text-gray-800 dark:text-white/90">{{ totals.total_manufacturers }}</h4>
+                        </div>
+                    </div>
                 </div>
+                <!-- Metric Item End -->
+
+                <!-- Metric Item Start -->
+                <div class="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03]">
+                    <p class="text-theme-sm text-gray-500 dark:text-gray-400">Total Locations</p>
+
+                    <div class="mt-3 flex items-end justify-between">
+                        <div>
+                            <h4 class="text-2xl font-bold text-gray-800 dark:text-white/90">{{ totals.total_locations }}</h4>
+                        </div>
+                    </div>
+                </div>
+                <!-- Metric Item End -->
             </div>
             <div
                 class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border"
